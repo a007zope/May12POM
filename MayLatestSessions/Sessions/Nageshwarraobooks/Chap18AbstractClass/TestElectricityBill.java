28a242c88da696c63eee6306cf8c6e2cda9f2fd5
P7111642 <P7111642@INAIRLT541343.ntil.com>
2023-05-07 09:03:53 +0530

    test elec bill code change

--- MayLatestSessions/Sessions/Nageshwarraobooks/Chap18AbstractClass/TestElectricityBill.java
+++ MayLatestSessions/Sessions/Nageshwarraobooks/Chap18AbstractClass/TestElectricityBill.java
@@ -1,13 +1,16 @@
 package Chap18AbstractClass;
 
 public class TestElectricityBill {
+	
+	/**
+	 * Calculating elec bill using abstract clas concept 
+	 * of abstract class Plan,subclass DomesticPlan, CommercialPlan
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 
 		Plan p;
-
-
-
 		DomesticPlan dp = new DomesticPlan();
 		CommercialPlan cp = new CommercialPlan();
 
